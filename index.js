9989b5b0cfbbeb7500c9523e8f144d403f8cef3a
CodeByMonir <monirhossen39682@gmail.com>
2026-06-27 11:27:20 +0600

    created the payments history, reviews control

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { ObjectId, MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = 5000;
 
@@ -32,83 +32,190 @@ client
   })
   .catch(console.dir);
 
-    const database = client.db("MediCare");
-    const doctorCollection = database.collection("doctors");
-    const appointmentCollection = database.collection("appointments");
-    const paymentCollection = database.collection("payments");
+const database = client.db("MediCare");
+const doctorCollection = database.collection("doctors");
+const appointmentCollection = database.collection("appointments");
+const paymentCollection = database.collection("payments");
+const reviewCollection = database.collection("reviews");
 
-    // created for public to visit doctors without login and only verified doctors will visible
-    app.get("/api/doctors", async (req, res) => {
-      const query = {};
-      if (req.query.verification) {
-        query.verification = req.query.verification;
-      }
+// created for public to visit doctors without login and only verified doctors will visible
+app.get("/api/doctors", async (req, res) => {
+  const query = {};
+  if (req.query.verification) {
+    query.verification = req.query.verification;
+  }
 
-      const cursor = doctorCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
+  const cursor = doctorCollection.find(query);
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+// created for public to visit doctors without login and only verified doctors will visible
+app.get("/api/doctor", async (req, res) => {
+  const query = {};
+  if (req.query.license) {
+    query.license = req.query.license;
+  }
+  const cursor = doctorCollection.find(query);
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+// created for doctors verification application.
+app.post("/api/doctors", async (req, res) => {
+  const doctor = req.body;
+  const result = await doctorCollection.insertOne(doctor);
+  res.send(result);
+});
+
+// created for doctors so they can see their profile
+app.get("/api/doctor/:doctorId", async (req, res) => {
+  const doctorId = req.params.doctorId;
+
+  const result = await doctorCollection.findOne({
+    doctorId: doctorId,
+  });
+
+  res.send(result);
+});
+
+// created for doctors so they can update their profile
+app.patch("/api/doctor/:doctorId", async (req, res) => {
+  const { doctorId } = req.params;
+  const updatedData = req.body;
+
+  const filter = { doctorId };
+
+  const updateDoc = {
+    $set: updatedData,
+  };
+
+  const result = await doctorCollection.updateOne(filter, updateDoc);
+
+  res.send(result);
+});
+
+// created for collect appointments.
+app.post("/api/appointments", async (req, res) => {
+  const appointments = req.body;
+  const result = await appointmentCollection.insertOne(appointments);
+  res.send(result);
+});
+
+// created for find doctor and patient appointments by their different doctorId and patientId.
+// URL format: /api/appointments?doctorId=123 OR /api/appointments?patientId=456
+app.get("/api/appointments", async (req, res) => {
+  try {
+    const { doctorId, patientId } = req.query;
+
+    let query = {};
+
+    if (doctorId || patientId) {
+      query.$or = [];
+      if (doctorId) query.$or.push({ doctorId: doctorId });
+      if (patientId) query.$or.push({ patientId: patientId });
+    }
+
+    const result = await appointmentCollection.find(query).toArray();
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: "Server error", error });
+  }
+});
+
+// URL format: PATCH -> /api/appointments?doctorId=123 OR /api/appointments?patientId=456
+app.patch("/api/appointments", async (req, res) => {
+  try {
+    const targetId = req.query.id || req.query._id;
+
+    const updateFields = req.body;
+
+    const query = { _id: new ObjectId(targetId) };
+
+    const result = await appointmentCollection.updateOne(query, {
+      $set: updateFields,
     });
 
-    // created for public to visit doctors without login and only verified doctors will visible
-    app.get("/api/doctor", async (req, res) => {
-      const query = {};
-      if (req.query.license) {
-        query.license = req.query.license;
-      }
-      const cursor = doctorCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
+    if (result.matchedCount === 0) {
+      return res
+        .status(404)
+        .send({ message: "No records found matching the criteria." });
+    }
 
-    // created for doctors verification application.
-    app.post("/api/doctors", async (req, res) => {
-      const doctor = req.body;
-      const result = await doctorCollection.insertOne(doctor);
-      res.send(result);
-    });
+    res.send(result);
+  } catch (error) {
+    console.error("Backend Error:", error);
+    res.status(500).send({ message: "Server error", error: error.message });
+  }
+});
 
-    // created for doctors so they can see their profile
-    app.get("/api/doctor/:doctorId", async (req, res) => {
-      const doctorId = req.params.doctorId;
+// created for payments collections.
+app.post("/api/payments", async (req, res) => {
+  const payments = req.body;
+  const result = await paymentCollection.insertOne(payments);
+  res.send(result);
+});
 
-      const result = await doctorCollection.findOne({
-        doctorId: doctorId,
-      });
+app.get("/api/payments", async (req, res) => {
+  try {
+    const { doctorId, patientId } = req.query;
 
-      res.send(result);
-    });
+    let query = {};
 
-    // created for doctors so they can update their profile
-    app.patch("/api/doctor/:doctorId", async (req, res) => {
-      const { doctorId } = req.params;
-      const updatedData = req.body;
+    if (doctorId || patientId) {
+      query.$or = [];
+      if (doctorId) query.$or.push({ doctorId: doctorId });
+      if (patientId) query.$or.push({ patientId: patientId });
+    }
 
-      const filter = { doctorId };
+    const result = await paymentCollection.find(query).toArray();
 
-      const updateDoc = {
-        $set: updatedData,
-      };
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: "Server error", error });
+  }
+});
 
-      const result = await doctorCollection.updateOne(filter, updateDoc);
+app.post("/api/reviews", async (req, res) => {
+  const review = req.body;
+  // console.log(review);
+  const result = await reviewCollection.insertOne(review);
+  res.send(result);
+});
 
-      res.send(result);
-    });
+app.get("/api/reviews", async (req, res) => {
+  try {
+    const { doctorId, patientId } = req.query;
 
-    // created for collect appointments.
-    app.post("/api/appointments", async (req, res) => {
-      const appointments = req.body;
-      const result = await appointmentCollection.insertOne(appointments);
-      res.send(result);
-    });
+    let query = {};
 
-    // created for payments collections.
-    app.post("/api/payments", async (req, res) => {
-      const payments = req.body;
-      const result = await paymentCollection.insertOne(payments);
-      res.send(result);
-    });
+    if (doctorId || patientId) {
+      query.$or = [];
+      if (doctorId) query.$or.push({ doctorId: doctorId });
+      if (patientId) query.$or.push({ patientId: patientId });
+    }
 
-    // don't know about you broh...
+    const result = await reviewCollection.find(query).toArray();
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: "Server error", error });
+  }
+});
+
+
+  app.delete("/api/reviews/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = {
+      _id: new ObjectId(id)
+    }
+
+    const result = await reviewCollection.deleteOne(query);
+    res.send(result);
+  })
+
+// don't know about you broh...
 
 //     await client.db("admin").command({ ping: 1 });
 //     console.log(
@@ -121,7 +228,7 @@ client
 // run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`app is running on port ${port}`)
-})
+  console.log(`app is running on port ${port}`);
+});
 
 module.exports = app;
